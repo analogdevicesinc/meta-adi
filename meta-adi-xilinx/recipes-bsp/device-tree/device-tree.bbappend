analogdevicesinc/meta-adi

commit 7841c24005025907bf4d79b54b31fce70c6e26b9

meta-adi-xilinx: device-tree: Refactor recipe
The motivation for this rework was to realize that the devicetrees
generated by petalinux based on our reference designs are not usable.
When using the petalinux devicetree, as is, and without any PL part (we
know already that we need to overwrite that part), we end up with a
system where, for example, no network devices are instantiated. So,
instead of just appending our devicetree to the petalinux one, resulting
in a lot of differences between the end devicetree and the ones we have
in our kernel tree, we completely overwrite it. However, we still
inlcude the petalinux pl.dtsi (so that user's can add IPs to our reference
designs and devicetree nodes are generated for those) and system-user.dtsi
(so that users can extend our devicetrees) in our top devicetree.
Like this, changes are kept to a minimal and sould only be introduced by
users and not by the build system.

Signed-off-by: Nuno Sá <nuno.sa@analog.com>

meta-adi-xilinx/recipes-bsp/device-tree/device-tree.bbappend
@@ -100,6 +100,7 @@ SRC_URI_append_microblaze = " \
 #	* vc707_fmcadc5
 KERNEL_DTB = "zynq-zed-adv7511-ad9361-fmcomms2-3"
 
+DTB_PL_DELETE = "pl-delete-nodes-${KERNEL_DTB}"
 # used for sanity check
 KERNEL_DTB_SUPPORTED_zynq = "zynq-zed-adv7511-ad9361-fmcomms2-3 \
 			zynq-zc706-adv7511-ad9434-fmc-500ebz \
@@ -166,11 +167,6 @@ DEVICETREE_PP_FLAGS += " \
 DEVICETREE_FLAGS += " \
 		-i${DTS_INCLUDE_PATH} \
 		"
-# set common variables
-set_common_vars() {
-	KERNEL_PL_DTB_FILE="${1}"
-	KERNEL_DTB_VER_FILE="${2}"
-}
 
 # Make sure that the selected device tree is supported on the current ${MACHINE}
 do_sanity_check() {
@@ -191,13 +187,14 @@ addtask do_sanity_check after do_patch before do_create_yaml
 
 # Based on the selected device tree, this function will:
 #	copy the device tree to ${WORKDIR}/system-user.dtsi since it is the one included by the top level device tree.
-#	In some case, we need to remove some /include/ linesto avoid duplication with the top level device tree used by petalinux.
-#	Remove /dts-v1/ duplicated line.
-#	Add the /include "pl-delete-nodes-*" to remove all the duplicated labels between ADI device trees and pl.dtsi (generated by petalinux).
+#	overwrite the ${DT_FILES_PATH}/system-top.dts with the selected devicetree.
+#	add the pl.dtsi so that IPs added to our reference designs are also included.
+#	Add the /include "pl-delete-nodes-*" to remove all the duplicated labels between ADI device trees and pl.dtsi.
+#	Include system-user.dtsi at the end of the devicetree so users can extend it.
 do_configure_append() {
 
-	[ ! -e "${WORKDIR}/pl-delete-nodes-${KERNEL_DTB}.dtsi" ] && \
-		{ bbfatal "Error: Could not find \"pl-delete-nodes-${KERNEL_DTB}.dtsi\" in \"${WORKDIR}\""; }
+	[ ! -e "${WORKDIR}/${DTB_PL_DELETE}.dtsi" ] && \
+		{ bbfatal "Error: Could not find \"${DTB_PL_DELETE}.dtsi\" in \"${WORKDIR}\""; }
 
 	# Used to see if the PL part is being compiled as an overlay! In that case, we cannot apply our normal workflow since it would fail!
 	# We try to keep the approach used by petalinux here, which is, if you build your PL as an overlay, then you should just have a
@@ -208,55 +205,24 @@ do_configure_append() {
 			bbwarn "PL is being built as an overlay but no user overlay was found! The default one generated by petalinux is being used..."
 			return 0
 		fi
-		echo "/include/ \"pl-delete-nodes-${KERNEL_DTB}.dtsi\"" >> "${DT_FILES_PATH}/pl.dtsi"
+		echo "/include/ \"${DTB_PL_DELETE}.dtsi\"" >> "${DT_FILES_PATH}/pl.dtsi"
 		echo "#include \"${DTS_OVERLAY}\"" >> "${DT_FILES_PATH}/pl.dtsi"
 
 		return 0
 	fi
 
-	if [ -e "${DTS_INCLUDE_PATH}/${KERNEL_DTB}.dts" ]; then
-		cp "${DTS_INCLUDE_PATH}/${KERNEL_DTB}.dts" ${WORKDIR}/analog-devices.dtsi
-	else
-		bbfatal "Error: Could not find selected device tree:\"${KERNEL_DTB}.dts\" in the kernel sources:\"${DTS_INCLUDE_PATH}\"!!"
-	fi
+	[ ! -e "${DTS_INCLUDE_PATH}/${KERNEL_DTB}.dts" ] && \
+		{ bbfatal "Error: Could not find selected device tree:\"${KERNEL_DTB}.dts\" in the kernel sources:\"${DTS_INCLUDE_PATH}\"!!"; }
 
-	case ${MACHINE} in
-		"plnx-zynq7")
-			# zynq has some corner cases that must be taken into account
-			if [ "${KERNEL_DTB}" == "zynq-zc706-adv7511-fmcdaq3-revC" ]; then
-				dtb_ver_file="${DTS_INCLUDE_PATH}/zynq-zc706-adv7511-fmcdaq3.dts"
-			elif [ "${KERNEL_DTB}" == "zynq-adrv9361-z7035-bob-cmos" ]; then
-				dtb_ver_file="${DTS_INCLUDE_PATH}/zynq-adrv9361-z7035-bob.dts"
-			elif [ "${KERNEL_DTB}" == "zynq-adrv9364-z7020-bob-cmos" ]; then
-				dtb_ver_file="${DTS_INCLUDE_PATH}/zynq-adrv9364-z7020-bob.dts"
-			else
-				dtb_ver_file="${WORKDIR}/analog-devices.dtsi"
-			fi
-			set_common_vars pl-delete-nodes-${KERNEL_DTB}.dtsi "${dtb_ver_file}"
-			[ -e "${DTS_INCLUDE_PATH}/zynq.dtsi" ] && {  \
-				sed -i s,[/#]include.*\"zynq-7000.dtsi\",, "${DTS_INCLUDE_PATH}/zynq.dtsi"; }
-
-			echo "#include \"zynq-stdout-path.dtsi\"" >> ${WORKDIR}/analog-devices.dtsi
-		;;
-		"plnx-zynqmp")
-			set_common_vars pl-delete-nodes-${KERNEL_DTB}.dtsi "${DTS_INCLUDE_PATH}/zynqmp-zcu102-revA.dts"
-			[ -e "${DTS_INCLUDE_PATH}/zynqmp-zcu102-revA.dts" ] && { \
-				sed -i 's,[/#]include.*\"zynqmp.dtsi\",,;s,[/#]include.*\"zynqmp-clk-ccf.dtsi\",,' "${DTS_INCLUDE_PATH}/zynqmp-zcu102-revA.dts"; }
-		;;
-		"plnx-microblazeel")
-			set_common_vars pl-delete-nodes-${KERNEL_DTB}.dtsi "${WORKDIR}/analog-devices.dtsi"
-		;;
-		*)
-			bbfatal "ERROR: Unsupported machine:${MACHINE}"
-		;;
-	esac
-
-	sed -i s,/dts-v1/\;,, "${KERNEL_DTB_VER_FILE}"
-	# If for some reason the WORKDIR is not cleaned we don't want to have
-	# multiple include lines for our dtsi file. We also remove system-conf
-	# since it touches in nodes that our dts already touch.
-	sed -i '/analog-devices.dtsi/d;/system-conf.dtsi/d' "${WORKDIR}/system-user.dtsi"
-	echo -e "#include \"analog-devices.dtsi\"" | cat - ${WORKDIR}/system-user.dtsi > temp && mv temp ${WORKDIR}/system-user.dtsi
-	echo -e "/include/ \"${KERNEL_PL_DTB_FILE}\"" | cat - ${WORKDIR}/analog-devices.dtsi > temp && mv temp ${WORKDIR}/analog-devices.dtsi
+	cp "${DTS_INCLUDE_PATH}/${KERNEL_DTB}.dts" "${DT_FILES_PATH}/system-top.dts"
+	# The next command will include the PL nodes after the first #include it finds...
+	sed -i '0,/#include.*/s//&\n#include "pl.dtsi"/' "${DT_FILES_PATH}/system-top.dts"
+	echo "/include/ \"${DTB_PL_DELETE}.dtsi\"" >> "${DT_FILES_PATH}/pl.dtsi"
+	[ ${MACHINE} == "plnx-zynq7" ] && { \
+		echo "#include \"zynq-stdout-path.dtsi\"" >> "${DT_FILES_PATH}/system-top.dts"; }
+	# Remove system-conf since it touches in nodes that our dts already touch.
+	sed -i '/system-conf.dtsi/d' "${WORKDIR}/system-user.dtsi"
+	# add the user dts extension
+	echo "#include \"system-user.dtsi\"" >> "${DT_FILES_PATH}/system-top.dts"
 }
 

meta-adi-xilinx/recipes-bsp/device-tree/files/pl-delete-nodes-fmcdaq2.dtsi
@@ -1,8 +1,4 @@
 /*Delete nodes from pl.dtsi which are redefined in ADI dts*/
-/ {
-	/delete-node/ aliases;
-};
-
 /delete-node/ &sys_mb;
 /delete-node/ &clk_cpu;
 /delete-node/ &clk_bus_0;

meta-adi-xilinx/recipes-bsp/device-tree/files/pl-delete-nodes-kc705_ad9467_fmc.dtsi
@@ -1,8 +1,4 @@
 /*Delete nodes from pl.dtsi which are redefined in ADI dts*/
-/ {
-	/delete-node/ aliases;
-};
-
 /delete-node/ &sys_mb;
 /delete-node/ &clk_cpu;
 /delete-node/ &clk_bus_0;

meta-adi-xilinx/recipes-bsp/device-tree/files/pl-delete-nodes-kc705_fmcjesdadc1.dtsi
@@ -1,8 +1,4 @@
 /*Delete nodes from pl.dtsi which are redefined in ADI dts*/
-/ {
-	/delete-node/ aliases;
-};
-
 /delete-node/ &sys_mb;
 /delete-node/ &clk_cpu;
 /delete-node/ &clk_bus_0;

meta-adi-xilinx/recipes-bsp/device-tree/files/pl-delete-nodes-kc705_fmcomms2-3.dtsi
@@ -1,8 +1,4 @@
 /*Delete nodes from pl.dtsi which are redefined in ADI dts*/
-/ {
-	/delete-node/ aliases;
-};
-
 /delete-node/ &sys_mb;
 /delete-node/ &clk_cpu;
 /delete-node/ &clk_bus_0;

meta-adi-xilinx/recipes-bsp/device-tree/files/pl-delete-nodes-kcu105_adrv9371x.dtsi
@@ -1,8 +1,4 @@
 /*Delete nodes from pl.dtsi which are redefined in ADI dts*/
-/ {
-	/delete-node/ aliases;
-};
-
 /delete-node/ &sys_mb;
 /delete-node/ &clk_cpu;
 /delete-node/ &clk_bus_0;

meta-adi-xilinx/recipes-bsp/device-tree/files/pl-delete-nodes-kcu105_fmcomms2-3.dtsi
@@ -1,8 +1,4 @@
 /*Delete nodes from pl.dtsi which are redefined in ADI dts*/
-/ {
-	/delete-node/ aliases;
-};
-
 /delete-node/ &sys_mb;
 /delete-node/ &clk_cpu;
 /delete-node/ &clk_bus_0;

meta-adi-xilinx/recipes-bsp/device-tree/files/pl-delete-nodes-vc707_fmcadc2.dtsi
@@ -1,8 +1,4 @@
 /*Delete nodes from pl.dtsi which are redefined in ADI dts*/
-/ {
-	/delete-node/ aliases;
-};
-
 /delete-node/ &sys_mb;
 /delete-node/ &clk_cpu;
 /delete-node/ &clk_bus_0;

commit 14c1bff287a9702541ea3abe52315d7bca4fbc8c

meta-adi-xilinx: devicetree: Fix microblaze build
For microblaze builds, the pl.dtsi cannot be included after the first
"#include.*" line it finds since it will lead to needed nodes to be
permanently deleted. Thus, it needs to be the first thing to be included
after the dts tag...

Fixes: 7841c24 ("meta-adi-xilinx: device-tree: Refactor recipe")
Signed-off-by: Nuno Sá <nuno.sa@analog.com>

meta-adi-xilinx/recipes-bsp/device-tree/device-tree.bbappend
@@ -214,8 +214,16 @@ do_configure_append() {
 		{ bbfatal "Error: Could not find selected device tree:\"${KERNEL_DTB}.dts\" in the kernel sources:\"${DTS_INCLUDE_PATH}\"!!"; }
 
 	cp "${DTS_INCLUDE_PATH}/${KERNEL_DTB}.dts" "${DT_FILES_PATH}/system-top.dts"
-	# The next command will include the PL nodes after the first #include it finds...
-	sed -i '0,/#include.*/s//&\n#include "pl.dtsi"/' "${DT_FILES_PATH}/system-top.dts"
+	# If we find a dts tag on the top devicetree, the pl.dtsi is included right after. This is specially important
+	# for microblaze projects where the pl.dtsi should be the first thing included. This, because there are nodes that are
+	# both defined in the pl.dtsi and in the base devicetree (eg: vc707.dtsi) and if we include the pl.dtsi after the base
+	# dts we might permanently delete nodes that are needed afterwards. For projects were we do not have a dts tag on the
+	# top devicetree (zynqMP), we include the pl.dtsi after the first #include it finds...
+	if grep -qse "/dts-v1/" "${DT_FILES_PATH}/system-top.dts"; then
+		sed -i '0,/\/dts-v1\/;/s//&\n#include "pl.dtsi"/' "${DT_FILES_PATH}/system-top.dts"
+	else
+		sed -i '0,/#include.*/s//&\n#include "pl.dtsi"/' "${DT_FILES_PATH}/system-top.dts"
+	fi
 	echo "/include/ \"${DTB_PL_DELETE}.dtsi\"" >> "${DT_FILES_PATH}/pl.dtsi"
 	# As it turns out, system-conf.dtsi has some autogenerated nodes that we can use. Specially, the chosen node since
 	# petalinux automatically generates proper bootargs depending on the project configuration (eg: use initramfs or sdcard as rootfs).