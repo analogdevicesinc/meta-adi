analogdevicesinc/meta-adi

commit 2f820fadfeb8fb3d4cd658c315788019687862f2

meta-adi-xilinx: devicetree: Fix microblaze build
For microblaze builds, the pl.dtsi cannot be included after the first
"#include.*" line it finds since it will lead to needed nodes to be
permanently deleted. Thus, it needs to be the first thing to be included
after the dts tag...

Fixes: 7841c24 ("meta-adi-xilinx: device-tree: Refactor recipe")
Signed-off-by: Nuno Sá <nuno.sa@analog.com>

meta-adi-xilinx/recipes-bsp/device-tree/device-tree.bbappend
@@ -214,8 +214,16 @@ do_configure_append() {
 		{ bbfatal "Error: Could not find selected device tree:\"${KERNEL_DTB}.dts\" in the kernel sources:\"${DTS_INCLUDE_PATH}\"!!"; }
 
 	cp "${DTS_INCLUDE_PATH}/${KERNEL_DTB}.dts" "${DT_FILES_PATH}/system-top.dts"
-	# The next command will include the PL nodes after the first #include it finds...
-	sed -i '0,/#include.*/s//&\n#include "pl.dtsi"/' "${DT_FILES_PATH}/system-top.dts"
+	# If we find a dts tag on the top devicetree, the pl.dtsi is included right after. This is specially important
+	# for microblaze projects where the pl.dtsi should be the first thing included. This, because there are nodes that are
+	# both defined in the pl.dtsi and in the base devicetree (eg: vc707.dtsi) and if we include the pl.dtsi after the base
+	# dts we might permanently delete nodes that are needed afterwards. For projects were we do not have a dts tag on the
+	# top devicetree (zynqMP), we include the pl.dtsi after the first #include it finds...
+	if grep -qse "/dts-v1/" "${DT_FILES_PATH}/system-top.dts"; then
+		sed -i '0,/\/dts-v1\/;/s//&\n#include "pl.dtsi"/' "${DT_FILES_PATH}/system-top.dts"
+	else
+		sed -i '0,/#include.*/s//&\n#include "pl.dtsi"/' "${DT_FILES_PATH}/system-top.dts"
+	fi
 	echo "/include/ \"${DTB_PL_DELETE}.dtsi\"" >> "${DT_FILES_PATH}/pl.dtsi"
 	# As it turns out, system-conf.dtsi has some autogenerated nodes that we can use. Specially, the chosen node since
 	# petalinux automatically generates proper bootargs depending on the project configuration (eg: use initramfs or sdcard as rootfs).

commit 3615fed49fe06afc3d5cbb3c031a586982b4e17b

meta-adi-xilinx: devicetree: Fix pl.dtsi inclusion
The only way to make sure that the pl.dtsi + pl-delete-nodes logic is
not breaking the selected devicetree is to include pl.dtsi right
after the devicetree tag. Hence, we can't really include pl.dtsi after
the #include directive since we cannot be sure that what's being
included by the first file does not define any PL nodes (which is the
case for some zynq projects). In those corner cases, the build might fail
or even worst, it might succeed but we end up with the nodes
autogenerated by petalinux which means that we won't probe our devices
when booting the HW.

Fixes: 2f820fa (meta-adi-xilinx: devicetree: Fix microblaze build)

Signed-off-by: Nuno Sá <nuno.sa@analog.com>

meta-adi-xilinx/recipes-bsp/device-tree/device-tree.bbappend
@@ -148,6 +148,11 @@ DTS_INCLUDE_PATH_zynq = "${STAGING_KERNEL_DIR}/arch/${ARCH}/boot/dts"
 DTS_INCLUDE_PATH_zynqmp = "${STAGING_KERNEL_DIR}/arch/${ARCH}/boot/dts/xilinx"
 DTS_INCLUDE_PATH_microblaze = "${STAGING_KERNEL_DIR}/arch/${ARCH}/boot/dts"
 
+# zynq has some corner case where this will be overwritten
+DTB_TAG_FILE = "${DT_FILES_PATH}/system-top.dts"
+# zynqMP always uses this devicetree (at least on the supported projects)
+DTB_TAG_FILE_zynqmp = "${DTS_INCLUDE_PATH}/zynqmp-zcu102-revA.dts"
+
 # Only used when FPGA_MANAGER is enabled. These are only some defaults. Note that, for example, for Microblaze
 # vc707.dts won't be a good choice if your platform is based on kc705 for instance...
 DTS_BASE_zynq ?= "${DTS_INCLUDE_PATH}/zynq-zc706"
@@ -191,6 +196,7 @@ addtask do_sanity_check after do_patch before do_create_yaml
 #	Add the /include "pl-delete-nodes-*" to remove all the duplicated labels between ADI device trees and pl.dtsi.
 #	Include system-user.dtsi at the end of the devicetree so users can extend it.
 do_configure_append() {
+	local dtb_tag_file=${DTB_TAG_FILE}
 
 	[ ! -e "${WORKDIR}/${DTB_PL_DELETE}.dtsi" ] && \
 		{ bbfatal "Error: Could not find \"${DTB_PL_DELETE}.dtsi\" in \"${WORKDIR}\""; }
@@ -214,15 +220,29 @@ do_configure_append() {
 		{ bbfatal "Error: Could not find selected device tree:\"${KERNEL_DTB}.dts\" in the kernel sources:\"${DTS_INCLUDE_PATH}\"!!"; }
 
 	cp "${DTS_INCLUDE_PATH}/${KERNEL_DTB}.dts" "${DT_FILES_PATH}/system-top.dts"
-	# If we find a dts tag on the top devicetree, the pl.dtsi is included right after. This is specially important
-	# for microblaze projects where the pl.dtsi should be the first thing included. This, because there are nodes that are
-	# both defined in the pl.dtsi and in the base devicetree (eg: vc707.dtsi) and if we include the pl.dtsi after the base
-	# dts we might permanently delete nodes that are needed afterwards. For projects were we do not have a dts tag on the
-	# top devicetree (zynqMP), we include the pl.dtsi after the first #include it finds...
-	if grep -qse "/dts-v1/" "${DT_FILES_PATH}/system-top.dts"; then
-		sed -i '0,/\/dts-v1\/;/s//&\n#include "pl.dtsi"/' "${DT_FILES_PATH}/system-top.dts"
-	else
-		sed -i '0,/#include.*/s//&\n#include "pl.dtsi"/' "${DT_FILES_PATH}/system-top.dts"
+
+	# corner cases
+	case "${KERNEL_DTB}" in
+	"zynq-zc706-adv7511-fmcdaq3-revC")
+		dtb_tag_file="${DTS_INCLUDE_PATH}/zynq-zc706-adv7511-fmcdaq3.dts"
+		;;
+	"zynq-adrv9361-z7035-bob-cmos")
+		dtb_tag_file="${DTS_INCLUDE_PATH}/zynq-adrv9361-z7035-bob.dts"
+		;;
+	"zynq-adrv9364-z7020-bob-cmos")
+		dtb_tag_file="${DTS_INCLUDE_PATH}/zynq-adrv9364-z7020-bob.dts"
+		;;
+	esac
+
+	# The only way to make sure that the pl.dtsi + pl-delete-nodes logic is not breaking the selected
+	# devicetree is to include pl.dtsi right after the devicetree tag. Hence, this logic is the first
+	# thing to be included. If there are errors, then [most likely] the problem should be in the pl-delete-nodes
+	# (or in the petalinux logic to autogenerate pl.dtsi).
+	# FIXME: For projects where the top devicetree does not hold the tag, this process will directly change the
+	# files on the fetched kernel tree. We should think of mechanism to avoid this or, at least, to bring the file
+	# to it's default after we are done...
+	if ! grep -qse "pl.dtsi" "${dtb_tag_file}"; then
+		sed -i '0,/\/dts-v1\/;/s//&\n#include "pl.dtsi"/' "${dtb_tag_file}"
 	fi
 	echo "/include/ \"${DTB_PL_DELETE}.dtsi\"" >> "${DT_FILES_PATH}/pl.dtsi"
 	# As it turns out, system-conf.dtsi has some autogenerated nodes that we can use. Specially, the chosen node since