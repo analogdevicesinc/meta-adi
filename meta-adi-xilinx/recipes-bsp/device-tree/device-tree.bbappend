analogdevicesinc/meta-adi

commit 6a404e346be47b0066713f8fe71026580b087f42

meta-adi-xilinx: device-tree: Minimize files to put in WORKDIR
There's no need to copy all the pl-delete-nodes-* files into
WORKDIR. If building for zynqmp, then we are sure that we won't need any
microblaze related file.

Signed-off-by: Nuno Sá <nuno.sa@analog.com>

meta-adi-xilinx/recipes-bsp/device-tree/device-tree.bbappend
@@ -1,52 +1,55 @@
 FILESEXTRAPATHS_prepend := "${THISDIR}/files:"
 
-SRC_URI += " \
-	file://pl-zynqmp-zcu102-rev10-ad9361-fmcomms2-3-overlay.dtsi \
-	file://pl-delete-nodes-zynq-zed-adv7511-ad9361-fmcomms2-3.dtsi \
-	file://pl-delete-nodes-zynq-zc706-adv7511-ad9434-fmc-500ebz.dtsi \
-	file://pl-delete-nodes-zynq-zc706-adv7511-fmcdaq2.dtsi \
-	file://pl-delete-nodes-zynq-zed-adv7511.dtsi \
-	file://pl-delete-nodes-zynq-zed-adv7511-ad9467-fmc-250ebz.dtsi \
-	file://pl-delete-nodes-zynq-zc706-adv7511.dtsi \
-	file://pl-delete-nodes-zynq-zc706-adv7511-adrv9009.dtsi \
-	file://pl-delete-nodes-zynq-zc706-adv7511-adrv9371.dtsi \
-	file://pl-delete-nodes-zynq-zc706-adv7511-ad6676-fmc.dtsi \
-	file://pl-delete-nodes-zynq-zc706-adv7511-ad9739a-fmc.dtsi \
-	file://pl-delete-nodes-zynq-zc706-adv7511-ad9625-fmcadc2.dtsi \
-	file://pl-delete-nodes-zynq-zc706-adv7511-ad9265-fmc-125ebz.dtsi \
-	file://pl-delete-nodes-zynq-zc706-adv7511-ad9361-fmcomms2-3.dtsi \
-	file://pl-delete-nodes-zynq-zc706-adv7511-ad9361-fmcomms5.dtsi \
-	file://pl-delete-nodes-zynq-zc706-adv7511-fmcomms11.dtsi \
-	file://pl-delete-nodes-zynq-zc706-adv7511-fmcdaq3-revC.dtsi \
-	file://pl-delete-nodes-zynq-zc706-adv7511-fmcjesdadc1.dtsi \
-	file://pl-delete-nodes-zynq-zed-imageon.dtsi \
-	file://pl-delete-nodes-zynq-adrv9361-z7035-bob-cmos.dtsi \
-	file://pl-delete-nodes-zynq-adrv9361-z7035-bob.dtsi \
-	file://pl-delete-nodes-zynq-adrv9361-z7035-fmc.dtsi \
-	file://pl-delete-nodes-zynq-adrv9364-z7020-bob-cmos.dtsi \
-	file://pl-delete-nodes-zynq-adrv9364-z7020-bob.dtsi \
-	file://pl-delete-nodes-zynq-zc702-adv7511-ad9361-fmcomms5.dtsi \
-	file://pl-delete-nodes-zynq-zc702-adv7511.dtsi \
-	file://pl-delete-nodes-zynqmp-zcu102-rev10-adrv9009.dtsi \
-	file://pl-delete-nodes-zynqmp-zcu102-rev10-fmcdaq2.dtsi \
-	file://pl-delete-nodes-zynqmp-zcu102-rev10-adrv9371.dtsi \
-	file://pl-delete-nodes-zynqmp-zcu102-rev10-ad9361-fmcomms2-3.dtsi \
-	file://pl-delete-nodes-zynqmp-zcu102-rev10-ad9361-fmcomms5.dtsi \
-	file://pl-delete-nodes-zynqmp-zcu102-rev10-fmcdaq3.dtsi \
-	file://pl-delete-nodes-fmcdaq2.dtsi \
-	file://pl-delete-nodes-kc705_fmcdaq2.dtsi \
-	file://pl-delete-nodes-kc705_ad9467_fmc.dtsi \
-	file://pl-delete-nodes-kc705_fmcomms2-3.dtsi \
-	file://pl-delete-nodes-kc705_fmcjesdadc1.dtsi \
-	file://pl-delete-nodes-kcu105_fmcdaq2.dtsi \
-	file://pl-delete-nodes-kcu105_adrv9371x.dtsi \
-	file://pl-delete-nodes-kcu105_fmcomms2-3.dtsi \
-	file://pl-delete-nodes-vc707_fmcadc2.dtsi \
-	file://pl-delete-nodes-vc707_fmcomms2-3.dtsi \
-	file://pl-delete-nodes-vc707_fmcjesdadc1.dtsi \
-	file://pl-delete-nodes-vc707_fmcadc5.dtsi"
+SRC_URI_append_zynq = " \
+		file://zynq-stdout-path.dtsi \
+		file://pl-zynqmp-zcu102-rev10-ad9361-fmcomms2-3-overlay.dtsi \
+		file://pl-delete-nodes-zynq-zed-adv7511-ad9361-fmcomms2-3.dtsi \
+		file://pl-delete-nodes-zynq-zc706-adv7511-ad9434-fmc-500ebz.dtsi \
+		file://pl-delete-nodes-zynq-zc706-adv7511-fmcdaq2.dtsi \
+		file://pl-delete-nodes-zynq-zed-adv7511.dtsi \
+		file://pl-delete-nodes-zynq-zed-adv7511-ad9467-fmc-250ebz.dtsi \
+		file://pl-delete-nodes-zynq-zc706-adv7511.dtsi \
+		file://pl-delete-nodes-zynq-zc706-adv7511-adrv9009.dtsi \
+		file://pl-delete-nodes-zynq-zc706-adv7511-adrv9371.dtsi \
+		file://pl-delete-nodes-zynq-zc706-adv7511-ad6676-fmc.dtsi \
+		file://pl-delete-nodes-zynq-zc706-adv7511-ad9739a-fmc.dtsi \
+		file://pl-delete-nodes-zynq-zc706-adv7511-ad9625-fmcadc2.dtsi \
+		file://pl-delete-nodes-zynq-zc706-adv7511-ad9265-fmc-125ebz.dtsi \
+		file://pl-delete-nodes-zynq-zc706-adv7511-ad9361-fmcomms2-3.dtsi \
+		file://pl-delete-nodes-zynq-zc706-adv7511-ad9361-fmcomms5.dtsi \
+		file://pl-delete-nodes-zynq-zc706-adv7511-fmcomms11.dtsi \
+		file://pl-delete-nodes-zynq-zc706-adv7511-fmcdaq3-revC.dtsi \
+		file://pl-delete-nodes-zynq-zc706-adv7511-fmcjesdadc1.dtsi \
+		file://pl-delete-nodes-zynq-zed-imageon.dtsi \
+		file://pl-delete-nodes-zynq-adrv9361-z7035-bob-cmos.dtsi \
+		file://pl-delete-nodes-zynq-adrv9361-z7035-bob.dtsi \
+		file://pl-delete-nodes-zynq-adrv9361-z7035-fmc.dtsi \
+		file://pl-delete-nodes-zynq-adrv9364-z7020-bob-cmos.dtsi \
+		file://pl-delete-nodes-zynq-adrv9364-z7020-bob.dtsi \
+		file://pl-delete-nodes-zynq-zc702-adv7511-ad9361-fmcomms5.dtsi \
+		file://pl-delete-nodes-zynq-zc702-adv7511.dtsi"
 
-SRC_URI_append_zynq = " file://zynq-stdout-path.dtsi"
+SRC_URI_append_zynqmp = " \
+		file://pl-delete-nodes-zynqmp-zcu102-rev10-adrv9009.dtsi \
+		file://pl-delete-nodes-zynqmp-zcu102-rev10-fmcdaq2.dtsi \
+		file://pl-delete-nodes-zynqmp-zcu102-rev10-adrv9371.dtsi \
+		file://pl-delete-nodes-zynqmp-zcu102-rev10-ad9361-fmcomms2-3.dtsi \
+		file://pl-delete-nodes-zynqmp-zcu102-rev10-ad9361-fmcomms5.dtsi \
+		file://pl-delete-nodes-zynqmp-zcu102-rev10-fmcdaq3.dtsi"
+
+SRC_URI_append_microblaze = " \
+		file://pl-delete-nodes-fmcdaq2.dtsi \
+		file://pl-delete-nodes-kc705_fmcdaq2.dtsi \
+		file://pl-delete-nodes-kc705_ad9467_fmc.dtsi \
+		file://pl-delete-nodes-kc705_fmcomms2-3.dtsi \
+		file://pl-delete-nodes-kc705_fmcjesdadc1.dtsi \
+		file://pl-delete-nodes-kcu105_fmcdaq2.dtsi \
+		file://pl-delete-nodes-kcu105_adrv9371x.dtsi \
+		file://pl-delete-nodes-kcu105_fmcomms2-3.dtsi \
+		file://pl-delete-nodes-vc707_fmcadc2.dtsi \
+		file://pl-delete-nodes-vc707_fmcomms2-3.dtsi \
+		file://pl-delete-nodes-vc707_fmcjesdadc1.dtsi \
+		file://pl-delete-nodes-vc707_fmcadc5.dtsi"
 
 # Set this variable with the desired device tree
 # Supported device tree files

commit c693ed286845b4250b6c4466ff909e4747c12d05

meta-adi-xilinx: Fix SRC_URI devicetree overlay example
The example to be used when dts overlays are enabled is based on zcu102.
Hence, if we want it to be uploaded to WORKDIR, it must be added to the
zynqmp SRC_URI override.

Fixes: 6a404e34 ("meta-adi-xilinx: device-tree: Minimize files to put in WORKDIR")
Signed-off-by: Nuno Sá <nuno.sa@analog.com>

meta-adi-xilinx/recipes-bsp/device-tree/device-tree.bbappend
@@ -1,7 +1,6 @@
 FILESEXTRAPATHS_prepend := "${THISDIR}/files:"
 
 SRC_URI_append_zynq = " \
-		file://pl-zynqmp-zcu102-rev10-ad9361-fmcomms2-3-overlay.dtsi \
 		file://pl-delete-nodes-zynq-zed-adv7511-ad9361-fmcomms2-3.dtsi \
 		file://pl-delete-nodes-zynq-zc706-adv7511-ad9434-fmc-500ebz.dtsi \
 		file://pl-delete-nodes-zynq-zc706-adv7511-fmcdaq2.dtsi \
@@ -36,6 +35,7 @@ SRC_URI_append_zynq = " \
 		file://pl-delete-nodes-zynq-zc702-adv7511.dtsi"
 
 SRC_URI_append_zynqmp = " \
+		file://pl-zynqmp-zcu102-rev10-ad9361-fmcomms2-3-overlay.dtsi \
 		file://pl-delete-nodes-zynqmp-zcu102-rev10-adrv9009-jesd204-fsm.dtsi \
 		file://pl-delete-nodes-zynqmp-zcu102-rev10-fmcdaq2.dtsi \
 		file://pl-delete-nodes-zynqmp-zcu102-rev10-adrv9371-jesd204-fsm.dtsi \