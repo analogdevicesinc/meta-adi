analogdevicesinc/meta-adi

commit cd042513d600883e000ead416d3b66920c199f3d

meta-adi-xilinx: libiio: remove lsb runtime dependency
In Petalinux 2020.1, lsb runtime functions are no longer present in the
core layer. Hence, this patch just patches the sysv init script so that
it is not dependent in these functions.

Signed-off-by: Nuno Sa <nuno.sa@analog.com>

meta-adi-xilinx/recipes-support/libiio/libiio_%.bbappend
@@ -1,12 +1,13 @@
+FILESEXTRAPATHS_prepend := "${THISDIR}/files:"
 BRANCH ?= "master"
 SRCREV = "${@ "565bf68eccfdbbf22cf5cb6d792e23de564665c7" if bb.utils.to_boolean(d.getVar('BB_NO_NETWORK')) else d.getVar('AUTOREV')}"
 SRC_URI_append = ";branch=${BRANCH}"
 PV = "0.21+git${SRCPV}"
+SRC_URI += "file://syvinitscript.patch"
 
 EXTRA_OECMAKE += "${@bb.utils.contains('DISTRO_FEATURES', 'sysvinit', '-DWITH_SYSVINIT=on', '', d)}"
 
 inherit update-rc.d
 
 INITSCRIPT_NAME = "${@bb.utils.contains('DISTRO_FEATURES', 'sysvinit', 'iiod', '', d)}"
-# it depends on lsb to run the iiod service
-RDEPENDS_${PN}-iiod += "${@bb.utils.contains('DISTRO_FEATURES', 'sysvinit', 'lsb', '', d)}"
+

meta-adi-xilinx/recipes-support/libiio/files/syvinitscript.patch
@@ -1,0 +1,69 @@
+diff --git a/iiod/init/iiod.init.cmakein b/iiod/init/iiod.init.cmakein
+index 2799925..9ca51d4 100644
+--- a/iiod/init/iiod.init.cmakein
++++ b/iiod/init/iiod.init.cmakein
+@@ -11,10 +11,14 @@
+ # Debian init.d script for the IIO Daemon
+ # Copyright (C) 2016 Analog Devices Inc.
+ 
+-. /lib/lsb/init-functions
++. /etc/init.d/functions
++
++set -e
+ 
+ # Server-side demuxing by default
+ IIOD_OPTS=-D
++DAEMON=@CMAKE_INSTALL_FULL_SBINDIR@/iiod
++PID=/var/run/iiod.pid
+ 
+ if test -f /etc/default/iiod; then
+     . /etc/default/iiod
+@@ -22,21 +26,16 @@ fi
+ 
+ case "$1" in
+ 	start)
+-		log_daemon_msg "Starting IIO Daemon" "iiod" || true
+-		if start-stop-daemon -S -b -q -m -p /var/run/iiod.pid -x @CMAKE_INSTALL_FULL_SBINDIR@/iiod -- $IIOD_OPTS; then
+-			log_end_msg 0 || true
+-		else
+-			log_end_msg 1 || true
+-		fi
++		echo -n "Starting IIO Daemon: "
++		start-stop-daemon -S -b -q -m -p $PID -x $DAEMON -- $IIOD_OPTS
++		echo "iiod"
+ 		;;
+ 
+ 	stop)
+-		log_daemon_msg "Stopping IIO Daemon" "iiod" || true
+-		if start-stop-daemon -K -q -p /var/run/iiod.pid; then
+-			log_end_msg 0 || true
+-		else
+-			log_end_msg 1 || true
+-		fi
++		echo -n "Stopping IIO Daemon: "
++		start-stop-daemon -K -q -p $PID
++		rm -f $PID
++		echo "iiod"
+ 		;;
+ 
+ 	restart|force-reload)
+@@ -45,15 +44,13 @@ case "$1" in
+ 		;;
+ 
+ 	status)
+-		if [ -f /var/run/iiod.pid ] ; then
+-			status_of_proc -p /var/run/iiod.pid @CMAKE_INSTALL_FULL_SBINDIR@/iiod iiod && exit 0 || exit $?
+-		else
+-			status_of_proc @CMAKE_INSTALL_FULL_SBINDIR@/iiod iiod && exit 0 || exit $?
+-		fi
++		# otherwise status will exit without any echo since pidof does not return 0
++		set +e
++		status $DAEMON
+ 		;;
+ 
+ 	*)
+-		log_action_msg "Usage: /etc/init.d/iiod.sh {start|stop|restart|status}" || true
++		echo "Usage: /etc/init.d/iiod.sh {start|stop|restart|status}"
+ 		exit 1
+ esac
+ 

commit c03279ac798fbadc719f65b82b5b5f5856b12bd1

meta-adi-xilinx: libiio: fix branch selection
Add the sysvinit patch in the same SRC_URI append line. Otherwise, the
'+=' operator has precedence over '_append' which means that the ';branch='
key will not be applied to the 'git://' URI and hence, we cannot specify
the branch to checkout. It will always use master no matter what is
given in BRANCH'.

Fixes: cd042513d600 ("meta-adi-xilinx: libiio: remove lsb runtime dependency")
Signed-off-by: Nuno Sá <nuno.sa@analog.com>

meta-adi-xilinx/recipes-support/libiio/libiio_%.bbappend
@@ -1,9 +1,8 @@
 FILESEXTRAPATHS_prepend := "${THISDIR}/files:"
 BRANCH ?= "master"
 SRCREV = "${@ "56f7db743651e02a838aab7bf224978da8661b22" if bb.utils.to_boolean(d.getVar('BB_NO_NETWORK')) else d.getVar('AUTOREV')}"
-SRC_URI_append = ";branch=${BRANCH}"
+SRC_URI_append = ";branch=${BRANCH} file://syvinitscript.patch"
 PV = "0.21+git${SRCPV}"
-SRC_URI += "file://syvinitscript.patch"
 
 EXTRA_OECMAKE += "${@bb.utils.contains('DISTRO_FEATURES', 'sysvinit', '-DWITH_SYSVINIT=on', '', d)}"
 # This is enabled by default if network_backend is enabled. But if zeroconf is not present, we cannot